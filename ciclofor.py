c218b448ef39dd15e17c3dfb74b378df2dc2d4ad
igris27 <andreydeivi@gmail.com>
2026-03-09 22:26:39 -0500

    agregando un comentario y editando el contenido que tenia

--- ciclofor.py
+++ ciclofor.py
@@ -2,4 +2,5 @@ tabla = int(input("Ingrese la tabla de multiplicar que desea: "))
 for x in range (1,11):
     print (f"{tabla}x {x} = {x*tabla}")
     #estoy haciendo un cambio
-    #este es otro cambio
+    #este es otro cambio (agregando un comentario)
+    #este es el cambio de igris
